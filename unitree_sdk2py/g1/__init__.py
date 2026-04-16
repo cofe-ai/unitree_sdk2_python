01bc693b7b6b9b6afd821df865e25e18b83b5802
Naitong Yu <ntyu@baai.ac.cn>
2026-04-16 15:07:32 +0800

    feat: add __init__.py for g1 module

---
+++ unitree_sdk2py/g1/__init__.py
@@ -0,0 +1,9 @@
+from .arm.g1_arm_action_client import G1ArmActionClient
+from .audio.g1_audio_client import AudioClient
+from .loco.g1_loco_client import LocoClient
+
+__all__ = [
+    "G1ArmActionClient",
+    "AudioClient",
+    "LocoClient",
+]
